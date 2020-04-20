2d8a84bb9103f724fb9214d8c327215a94b7fbac
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-20 20:39:59 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require "pry"
+class Song 
+  @@all=[]
+  attr_accessor :name, :artist
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+  @@all 
+  end
+  
+  def artist_name
+      if artist
+        return self.artist.name 
+      else 
+        return nil 
+      end
+  end 
+end 
